44d64dc6b77dd5a7d49e9c6629944ba2ace2530d
Marcelo Oliveira <celocmoliveira@gmail.com>
2021-09-08 23:20:18 -0300

    Criado o HTML, CSS, JS e feito o primeiro exercicio.

---
+++ fundamentos/bloco5-javascript-dom-eventos-web-storage/dia3-javascript-eventos/script.js
@@ -0,0 +1,30 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+  
+  // Escreva seu código abaixo.
+//   O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+let dezDayList = [29, 30, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let days = document.querySelector('#days'); 
+
+function criarNumberDays (){
+    for (let key in dezDayList){
+    let numberDays = document.createElement('li');
+        numberDays.innerHTML = dezDayList[key];
+        numberDays.classList.add('day');
+        days.appendChild(numberDays);
+    };
+};
+criarNumberDays();
